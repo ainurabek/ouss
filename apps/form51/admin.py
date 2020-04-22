cbd01904da290a99ed0bdbe9f7526b875ab687cc
AlymbekovvDastan <alymbek_uulu_d@iuca.kg>
2020-04-22 19:17:30 +0600

    Customer

--- apps/form51/admin.py
+++ apps/form51/admin.py
@@ -1,8 +1,9 @@
 from django.contrib import admin
 
-from .models import Region, Form51Location
+from .models import Region, Form51Location, Form51
 
 
 admin.site.register(Region)
 admin.site.register(Form51Location)
+admin.site.register(Form51)
 
