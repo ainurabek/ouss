06e0af4deb3d6487e1c4fad982881c8b9e940148
ainurabek <ainurabektemir@gmail.com>
2020-06-08 14:26:43 +0600

    form51

--- apps/opu/form51/models.py
+++ apps/opu/form51/models.py
@@ -8,13 +8,13 @@ from apps.opu.objects.models import Object
 
 class Form51(models.Model):
     """Форма 5.1"""
-    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name="Примечание (№ID, МН, Аренда)")
-    object = models.ForeignKey(Object, on_delete=models.CASCADE, verbose_name="КО")
-    num_ouss = models.CharField("Номер распоряжения ОУСС", max_length=250)
-    order = models.ImageField('Распоряжение', upload_to='object/order/', blank=True)
-    schema = models.ImageField('Схема', upload_to='object/schema/', blank=True)
-    reserve = models.CharField('Резерва потока', max_length=15)
-    reserve_object = models.ManyToManyField(Object, verbose_name="Трасса резерва потока", related_name="reserve_objects", blank=True, null=True)
+    customer = models.ForeignKey(Customer, on_delete=models.CASCADE, verbose_name="Примечание (№ID, МН, Аренда)", blank=True, null=True)
+    object = models.ForeignKey(Object, on_delete=models.CASCADE, verbose_name="КО", blank=True, null=True)
+    num_ouss = models.CharField("Номер распоряжения ОУСС", max_length=250, blank=True, null=True)
+    order = models.ImageField('Распоряжение', upload_to='object/order/', blank=True, null=True)
+    schema = models.ImageField('Схема', upload_to='object/schema/', blank=True, null=True)
+    reserve = models.CharField('Резерва потока', max_length=15, blank=True, null=True)
+    reserve_object = models.ManyToManyField(Object, verbose_name="Трасса резерва потока", related_name="reserve_objects", blank=True)
     report_num = models.CharField('Номер донесения', max_length=200, blank=True, null=True)
     created_by = models.ForeignKey(Profile, on_delete=models.CASCADE, blank=True, null=True)
 
