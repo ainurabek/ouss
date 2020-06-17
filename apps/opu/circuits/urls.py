07a846eb7b8bfd5da530ddc093af28c0ec1c6389
ainurabek <ainurabektemir@gmail.com>
2020-06-17 15:11:17 +0600

    circ_api

--- apps/opu/circuits/urls.py
+++ apps/opu/circuits/urls.py
@@ -1,19 +1,17 @@
 from django.conf.urls import url
 from django.urls import path, include
 from rest_framework import routers
-from apps.opu.circuits.views import CircuitViewSet, CircuitEditView
+from apps.opu.circuits.views import CircuitListViewSet, CircuitEditView
 from . import views
 
 app_name = 'circuits'
 
 router = routers.DefaultRouter()
-router.register('', CircuitViewSet, basename='circuit')
-
 
 
 
 urlpatterns = [
-
+    path('trakt/<int:pk>/', CircuitListViewSet.as_view(), name = 'circuit_list '),
     path('edit/<int:pk>/', CircuitEditView.as_view(), name='circuit_edit'),
     path('', include(router.urls))
 
--- apps/opu/circuits/views.py
+++ apps/opu/circuits/views.py
@@ -7,14 +7,37 @@ from rest_framework.response import Response
 
 from apps.opu.objects.models import Object
 from rest_framework.views import APIView
+from django.http import Http404
+from rest_framework.filters import SearchFilter
+from django_filters.rest_framework import DjangoFilterBackend
 
+# class CircuitViewSet(viewsets.ModelViewSet):
+#     queryset = Circuit.objects.all()
+#     serializer_class = CircuitList
+#     lookup_field = 'pk'
+#     authentication_classes = (TokenAuthentication,)
+#     permission_classes = (IsAuthenticatedOrReadOnly,)
 
-class CircuitViewSet(viewsets.ModelViewSet):
-    queryset = Circuit.objects.all()
-    serializer_class = CircuitList
-    lookup_field = 'pk'
-    authentication_classes = (TokenAuthentication,)
+class CircuitListViewSet(APIView):
     permission_classes = (IsAuthenticatedOrReadOnly,)
+    authentication_classes = (TokenAuthentication,)
+    filter_backends = (SearchFilter, DjangoFilterBackend)
+    search_fields = ('num_circuit', 'name', 'type_using', 'category', 'num_order', 'date_order',
+                     'num_arenda', 'speed', 'measure', 'point1', 'point2', 'customer', 'id_object', 'mode', 'type_com')
+    filterset_fields = ('point1', 'point2', 'customer', 'id_object', 'category')
+
+    def get_object(self, pk):
+        try:
+            return Object.objects.get(pk=pk)
+        except Object.DoesNotExist:
+            raise Http404
+
+    def get(self, request, pk):
+        trakt = self.get_object(pk)
+        circuit = Circuit.objects.filter(id_object=trakt.pk)
+        data = CircuitList(circuit, many=True).data
+        return Response(data)
+
 
 class CircuitEditView(generics.RetrieveUpdateAPIView):
     lookup_field = 'pk'
@@ -25,3 +48,4 @@ class CircuitEditView(generics.RetrieveUpdateAPIView):
 
     def perform_update(self, serializer):
         serializer.save(created_by=self.request.user.profile)
+
