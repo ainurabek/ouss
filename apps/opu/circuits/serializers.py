380b1a97aa7b8ec32486f397982dfe616a9d2076
ainurabek <ainurabektemir@gmail.com>
2020-06-17 12:06:45 +0600

    circ_from53

--- apps/opu/circuits/serializers.py
+++ apps/opu/circuits/serializers.py
@@ -1,40 +1,124 @@
 from rest_framework import serializers
 from django.contrib.auth import get_user_model, authenticate
-from .models import Circuit
-from ..objects.models import Category
+from .models import Circuit, TypeCom, Mode, Speed, Measure
+from ..customer.models import Customer
+from ..objects.models import Category, Object, Point, InOut
 
 User = get_user_model()
 
 
-class CircuitList(serializers.ModelSerializer):
-    class Meta:
-        model = Circuit
-        fields = ('id', 'name', 'number', 'id_object', 'num_circuit', 'final_destination', 'type_using', 'category', 'num_order',
-                  'date_order', 'num_arenda', 'speed', 'measure', 'adding', 'comments',
-                  'type_transit1', 'type_transit2', 'transit', 'transit2', 'in_out', 'first', 'point1', 'point2',
-                  'id_object', 'customer', 'mode', 'type_com')
+class ObjectCircSerializer(serializers.ModelSerializer):
 
+    class Meta:
+        model = Object
+        fields = ('__all__')
+
+
+class PointCircSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Point
+        fields = ('id', 'name', 'point')
+
+class TypeComSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = TypeCom
+        fields = ('id', 'name')
+
+class SpeedCircSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Speed
+        fields = ('id', 'name')
+
+class MeasureCircSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Measure
+        fields = ('id', 'name')
+
+class InOutSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = InOut
+        fields = ('id', 'name')
+
+class ModeCircSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        model = Mode
+        fields = ('id', 'name')
 
 class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         model = Category
         fields = '__all__'
 
+class CustomerSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Customer
+        fields = '__all__'
 
-class CircuitEdit(serializers.ModelSerializer):
-
+class TransitCircSerializer(serializers.ModelSerializer):
+    point1 = serializers.SlugRelatedField(slug_field='point', read_only=True)
+    point2 = serializers.SlugRelatedField(slug_field='point', read_only=True)
 
     class Meta:
         model = Circuit
-        fields = ('num_circuit', 'final_destination', 'type_using', 'category', 'num_order',
+        fields = ('id', 'point1', 'name', 'point2')
+
+class CircuitList(serializers.ModelSerializer):
+    id_object = ObjectCircSerializer()
+    point1=PointCircSerializer()
+    point2=PointCircSerializer()
+    transit = TransitCircSerializer(many=True)
+    transit2 = TransitCircSerializer(many=True)
+    type_com = TypeComSerializer()
+    customer = CustomerSerializer()
+    mode = ModeCircSerializer()
+    in_out=InOutSerializer()
+    speed = SpeedCircSerializer()
+    measure = MeasureCircSerializer()
+    category = CategorySerializer()
+    class Meta:
+        model = Circuit
+        fields = ('id', 'name', 'number', 'id_object', 'num_circuit', 'type_using', 'category', 'num_order',
                   'date_order', 'num_arenda', 'speed', 'measure', 'adding', 'comments',
-                  'type_transit1', 'type_transit2', 'transit', 'transit2', 'in_out', 'first', 'point1',
-                  'point2', 'customer', 'mode', 'type_com')
+                   'transit', 'transit2', 'in_out', 'first', 'point1', 'point2',
+                  'customer', 'mode', 'type_com')
+
+
+
+
+
+class CircuitEdit(serializers.ModelSerializer):
+    class Meta:
+        model = Circuit
+        fields = ('number', 'type_using', 'category', 'num_order',
+                  'date_order', 'num_arenda', 'speed', 'measure', 'adding', 'comments',
+                   'in_out', 'first', 'point1', 'point2',
+                  'customer', 'mode', 'type_com')
 
     def update(self, instance, validated_data):
         """
         Update and return an existing `Snippet` instance, given the validated data.
         """
+        instance.number = validated_data.get('number', instance.number)
+        instance.type_using = validated_data.get('type_using', instance.type_using)
+        instance.num_order = validated_data.get('num_order', instance.num_order)
+        instance.date_order = validated_data.get('date_order', instance.date_order)
+        instance.num_arenda = validated_data.get('num_arenda', instance.num_arenda)
+        instance.speed = validated_data.get('speed', instance.speed)
+        instance.measure = validated_data.get('measure', instance.measure)
+        instance.adding = validated_data.get('adding', instance.adding)
+        instance.comments = validated_data.get('comments', instance.comments)
+        instance.in_out = validated_data.get('in_out', instance.in_out)
+        instance.customer = validated_data.get('customer', instance.customer)
+        instance.mode = validated_data.get('mode', instance.mode)
+        instance.type_com = validated_data.get('type_com', instance.type_com)
+
+
         instance.category = validated_data.get('category', instance.category)
         instance.point1 = validated_data.get('point1', instance.point1)
         instance.point2 = validated_data.get('point2', instance.point2)
--- apps/opu/form53/serializers.py
+++ apps/opu/form53/serializers.py
@@ -11,13 +11,7 @@ from apps.opu.form53.models import Form53
 
 from apps.opu.circuits.models import Circuit
 
-
-class PointForm51Serializer(serializers.ModelSerializer):
-
-    class Meta:
-        model = Point
-        fields = ("name", "point")
-
+from apps.opu.circuits.serializers import CircuitList
 
 class Form53CreateSerializer(serializers.ModelSerializer):
     """Создания Формы 5.3"""
@@ -26,26 +20,9 @@ class Form53CreateSerializer(serializers.ModelSerializer):
         model = Form53
         fields = ("id",  "order", "schema")
 
-
-class CircuitForm53Serializer(serializers.ModelSerializer):
-    transit = TransitSerializer(many=True)
-    transit2 = TransitSerializer(many=True)
-    point1 = PointForm51Serializer()
-    point2 = PointForm51Serializer()
-
-    class Meta:
-        model = Circuit
-        fields = ('num_circuit', 'final_destination', 'name', 'type_using', 'category', 'num_order',
-                  'date_order', 'num_arenda', 'speed', 'measure', 'adding', 'comments',
-                  'type_transit1', 'type_transit2', 'transit', 'transit2', 'in_out', 'first', 'point1',
-                  'point2', 'customer', 'mode', 'type_com', 'id_object')
-
-
 class Form53Serializer(serializers.ModelSerializer):
     """Список Формы 5.3"""
-    circuit = CircuitForm53Serializer()
-
-
+    circuit = CircuitList()
     class Meta:
         model = Form53
         fields = ("id", "circuit",  "order", "schema")
