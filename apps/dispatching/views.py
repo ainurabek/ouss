3e17a61cb833f8e604c9e91a98d45b94cf6ba7e8
ainurabek <ainurabektemir@gmail.com>
2020-09-14 16:40:03 +0600

    date_from_t

--- apps/dispatching/views.py
+++ apps/dispatching/views.py
@@ -213,10 +213,11 @@ class EventUpdateAPIView(UpdateAPIView):
         instance = serializer.save()
         if instance.id_parent is not None:
             latest_event = instance.id_parent.event_id_parent.all().latest()
-            if latest_event.pk == instance.pk and instance.date_from != date_to:
+            if latest_event.pk == instance.pk and str(instance.date_from) != date_to:
+
                 instance.id_parent.date_to =instance.date_from
                 instance.id_parent.save()
-            if latest_event.pk ==instance.pk and instance.index1 != index1:
+            if latest_event.pk ==instance.pk and str(instance.index1) != index1:
                 instance.id_parent.index1=instance.index1
                 instance.id_parent.save()
             if instance.previous is not None:
