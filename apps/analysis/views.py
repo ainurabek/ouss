3568a117d4423f2764d91ef6c3ff9ae8cb7be688
ainurabek <ainurabektemir@gmail.com>
2020-09-23 11:56:12 +0600

    analysis_disp_event_list_view

--- apps/analysis/views.py
+++ apps/analysis/views.py
@@ -103,31 +103,28 @@ def get_report(request):
 class DispEvent1ListAPIView(viewsets.ModelViewSet):
     permission_classes = (IsAuthenticatedOrReadOnly,)
     authentication_classes = (TokenAuthentication,)
-    queryset = Event.objects.filter(callsorevent=True)
+    queryset = Event.objects.filter(callsorevent=False, index1__id=8)
     lookup_field = 'pk'
     serializer_class = DispEvent1ListSerializer
     filter_backends = (SearchFilter, DjangoFilterBackend)
-    filterset_fields = ('object', 'ips', 'circuit',)
+    filterset_fields = ('object', 'ips', 'circuit', 'responsible_outfit',)
 
     def get_queryset(self):
         today = datetime.date.today()
-        queryset = self.queryset.filter(created_at=today,  index1__id=8, callsorevent=False)
+        self.queryset = self.queryset.filter(created_at=today)
 
-        responsible_outfit = self.request.query_params.get('responsible_outfit', None)
+
         date_from = self.request.query_params.get('date_from', None)
         date_to = self.request.query_params.get('date_to', None)
 
-
-        if responsible_outfit is not None and responsible_outfit != '':
-            queryset = self.queryset.filter(responsible_outfit=responsible_outfit, index1__id=8)
         if date_to == "" and date_from != '':
-            queryset = self.queryset.filter(created_at=date_from, index1__id=8)
+            self.queryset = self.queryset.filter(created_at=date_from)
         elif date_to != '' and date_from == '':
-            queryset = self.queryset.filter(created_at=date_to, index1__id=8)
+            self.queryset = self.queryset.filter(created_at=date_to)
         elif date_to != '' and date_from != '':
-            queryset = self.queryset.filter(created_at__gte=date_from, created_at__lte=date_to, index1__id=8)
+            self.queryset = self.queryset.filter(created_at__gte=date_from, created_at__lte=date_to)
 
-        return queryset
+        return self.queryset
 
 # class DispEventHistory(viewsets.ModelViewSet):
 #     permission_classes = (IsAuthenticatedOrReadOnly,)
