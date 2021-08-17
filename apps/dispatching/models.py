86d47e5c5dfaa3831096575d9cbf2ae708bbde4b
Alymbek uulu Dastan <alymbek.uulu.d@gmail.com>
2021-08-17 17:08:25 +0600

    Test commit 2

--- apps/dispatching/models.py
+++ apps/dispatching/models.py
@@ -1,7 +1,4 @@
 from django.db import models
-from django.db.models.signals import pre_save
-from django.dispatch import receiver
-
 from apps.accounts.models import Profile
 from apps.opu.circuits.models import Circuit
 from apps.opu.customer.models import Customer
